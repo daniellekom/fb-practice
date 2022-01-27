a5fe45033e8a66cf0583415a69d1812ce4229490
Danielle Kom <daniellekom1@gmail.com>
2022-01-27 11:45:47 -0500

    credentials file.json

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 //shift up arrow to select w out curly brace 
 //then option shift down arrow it will copy it all down 
 //created 4 restaurants easier this way
-//restaurants.json folder
+//from restaurants.json folder
 
 
 
-//import our restaurants how do u import? require ('./')
+// 1) import our restaurants how do u import? require ('./')
 //then go to beginning of line and add variable const restaurants =
 
 const restaurants = require('./restaurants.json');
-//check if it works by running first restaurant 
-console.log(restaurants[0].name)
+// console.log(restaurants[0].name) then delete to check if it runs from .json
+
+// 2)import a set of library tools from google(firebase library) to talk to firebase and Firestore
+const { initializeApp, applicationDefault, cert} = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue} = require('firebase-admin/firestore');
+
+// use npm by terminal npm init -y for yes to get package.json
+// then in terminal npm install firebase-admin and node_modules will appear on left with(x amount of packages)
+// then create a file .gitignore outside of node_module and inside node_modules
+//then go on firebase and go to ur project settings(jeremiahs notes)
+//download the private key and then drag it to ur vscode and rename it to credntials.json and add it to ur gitignore file
+//so ur private key is now public (if u want it to be public)
 
 
+// 3) connect to Firestore ("./")= means my own file
+const credntials = require("./credentials.json")
 
-//connect to Firestore
 
-//create a collection called "restaurants"
+// 4) create a collection called "restaurants"
 
-//add each restaurant
+
+// 5) add each restaurant
